d246da8a056be032525bf7fad8809d70ee68c5b1
safinhr <hrsafin2434@gmail.com>
2023-12-24 11:01:41 +0600

    react hook : useState() with object

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -2,16 +2,22 @@ import { Button } from '@material-tailwind/react';
 import React, { useState } from 'react';
 
 const Home = () => {
-    const [counter, setCounter] = useState(0)
-    const [name, setName] = useState("your text")
+    const [details, setDetails] = useState({counter : 0, name : "your text"})
     const handleIncrease = () => {
-        setCounter(counter + 1)
+        setDetails((prev) => {
+            return {
+                ...prev,
+                counter : details.counter + 1
+            }
+        })
     }
+
+    console.log(details)
     
     return (
         <div className=' text-3xl flex flex-col gap-3 justify-center items-center h-screen'>
             <input className=' border-2' onBlur={(e) => setName(e.target.value)} type="text" />
-            <h2>{name} has click {counter} </h2>
+            <h2>{details.name} has click {details.counter} </h2>
             <Button onClick={handleIncrease}>Increase</Button>
         </div>
     );
