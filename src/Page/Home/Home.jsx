0920dfc13392b54c493c57672b61e46e5305d2ff
safinhr <hrsafin2434@gmail.com>
2023-12-24 10:44:04 +0600

    reat Hook : useState() basic

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import { Button } from '@material-tailwind/react';
+import React, { useState } from 'react';
 
 const Home = () => {
+    const [counter, setCounter] = useState(0)
+    const handleIncrease = () => {
+        setCounter(counter + 1)
+    }
+    
     return (
-        <div className=' text-3xl flex justify-center items-center h-screen'>
-            this is home
+        <div className=' text-3xl flex flex-col gap-3 justify-center items-center h-screen'>
+            <h2>Counter : {counter}</h2>
+            <Button onClick={handleIncrease}>Increase</Button>
         </div>
     );
 };
