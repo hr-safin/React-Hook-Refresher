1e752f34288495014228d9fabee063d70d6f1e4a
safinhr <hrsafin2434@gmail.com>
2023-12-24 19:54:41 +0600

    practiced useReducer()

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -2,48 +2,49 @@
 import { Button } from '@material-tailwind/react';
 import React, {  useReducer, useState } from 'react';
 
+const initialState = { count : 0}
 
 const ACTION = {
-    INCREASE : "increase",
+    INCREASE  : "increase",
     DECREASE : "decrease"
 }
-const initialState = {count : 0}
 
-const reducer = (state, action) => {
-    console.log(action)
+const reducer = (state,action) => {
+    
     switch(action.type){
-
-        case ACTION.INCREASE : 
-            return {count : state.count +  1}
-        
-        case ACTION.DECREASE : 
-            return {count : state.count -  1}
-
+     
+        case ACTION.INCREASE:
+            return {count : state.count + 1}  
+        case ACTION.DECREASE: 
+            return {count : state.count - 1}
+            
         default:
             return state
     }
-    
+
 }
 const Home = () => {
-    
-    // const [count, setCount] = useState(0)
 
-    const [state, dispatch]  = useReducer(reducer, initialState )
+    const [state, dispatch] = useReducer(reducer, initialState)
 
-    const increaseCount = () => {
+
+    const handleIncrease = () => {
         dispatch({type : ACTION.INCREASE})
     }
 
-    const decreaseCount = () => {
+    const handleDecrease = () => {
         dispatch({type : ACTION.DECREASE})
     }
+    
+    
 
     return (
         <div className=' text-3xl flex flex-col   gap-3 justify-center items-center h-screen'>
+            
             <h2>Count : {state.count}</h2>
-            <div className=' flex gap-5'>
-            <Button onClick={increaseCount}  color='green'>Increase</Button>
-            <Button onClick={decreaseCount}  color='red'>Decrease</Button>
+            <div>
+                <Button onClick={handleIncrease} color='green'>Increase</Button>
+                <Button onClick={handleDecrease} color="red">Decrease</Button>
             </div>
             
         </div>
