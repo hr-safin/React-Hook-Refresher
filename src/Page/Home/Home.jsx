bd68a772fbf5e86093fd4855d3f21369f65a43d6
safinhr <hrsafin2434@gmail.com>
2023-12-24 15:35:05 +0600

    react hook : useRef()

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,8 +1,10 @@
-import { Button } from '@material-tailwind/react';
-import React, { useEffect, useState } from 'react';
+
+import React, { useEffect, useRef, useState } from 'react';
 
 const Home = () => {
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
+    const inputElement = useRef(0)
     // const [details, setDetails] = useState({counter : 0, name : "your text"})
     // const [otherCount, setOtherCount] = useState(5)
     // const handleIncrease = () => {
@@ -24,28 +26,22 @@ const Home = () => {
     //     setOtherCount(otherCount + 5)
     // }
 
-    useEffect(() => {
-        
-        console.log("Run useEffect", count)
 
-        return () => {
-            console.log("Clean Up", count)
-        }
-    }, [count])
+    const handleRef = () => {
+        console.log(inputElement)
+        inputElement.current.style.width = "800px"
+        inputElement.current.style.height = "200px"
+        inputElement.current.focus()
+    }
+    
 
 
 
     
     return (
         <div className=' text-3xl flex flex-col gap-3 justify-center items-center h-screen'>
-            {/* <input className=' border-2' onBlur={(e) => setName(e.target.value)} type="text" />
-            <h2>{details.name} has click {details.counter} </h2>
-            <Button onClick={handleIncrease}>Increase</Button>
-            <h2>{details.name} has click {otherCount} </h2>
-            <Button onClick={handleIncrease2}>Increase</Button> */}
-
-            <h2>Count : {count}</h2>
-            <Button onClick={() => setCount(count + 1)}>Increase</Button>
+            <input className=' border-2' type="text"  ref={inputElement} />
+           <button className=' py-2 px-3 rounded-md text-white bg-black' onClick={handleRef} >Click Me</button>
         </div>
     );
 };
