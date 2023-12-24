82578c12fe88cf9df5c78c3c5966e4189db3d0f8
safinhr <hrsafin2434@gmail.com>
2023-12-24 19:43:24 +0600

    react hook : useReducer()

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,22 +1,46 @@
 
 import { Button } from '@material-tailwind/react';
-import React, {  useState } from 'react';
+import React, {  useReducer, useState } from 'react';
 
+
+const ACTION = {
+    INCREASE : "increase",
+    DECREASE : "decrease"
+}
+const initialState = {count : 0}
+
+const reducer = (state, action) => {
+    console.log(action)
+    switch(action.type){
+
+        case ACTION.INCREASE : 
+            return {count : state.count +  1}
+        
+        case ACTION.DECREASE : 
+            return {count : state.count -  1}
+
+        default:
+            return state
+    }
+    
+}
 const Home = () => {
     
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
+
+    const [state, dispatch]  = useReducer(reducer, initialState )
 
     const increaseCount = () => {
-        setCount(prev => prev + 1)
+        dispatch({type : ACTION.INCREASE})
     }
 
     const decreaseCount = () => {
-        setCount(prev => prev -1)
+        dispatch({type : ACTION.DECREASE})
     }
 
     return (
         <div className=' text-3xl flex flex-col   gap-3 justify-center items-center h-screen'>
-            <h2>Count : {count}</h2>
+            <h2>Count : {state.count}</h2>
             <div className=' flex gap-5'>
             <Button onClick={increaseCount}  color='green'>Increase</Button>
             <Button onClick={decreaseCount}  color='red'>Decrease</Button>
