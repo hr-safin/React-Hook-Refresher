4c204af527fa84e1db3e5c229dab9222c72c96d1
safinhr <hrsafin2434@gmail.com>
2023-12-24 12:26:09 +0600

    cleanUp function in useEffect() hook

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -2,39 +2,50 @@ import { Button } from '@material-tailwind/react';
 import React, { useEffect, useState } from 'react';
 
 const Home = () => {
-    const [details, setDetails] = useState({counter : 0, name : "your text"})
-    const [otherCount, setOtherCount] = useState(5)
-    const handleIncrease = () => {
-        setDetails((prev) => {
-            return {
-                ...prev,
-                counter : details.counter + 1
-            }
-        })
-    }
+    const [count, setCount] = useState(0)
+    // const [details, setDetails] = useState({counter : 0, name : "your text"})
+    // const [otherCount, setOtherCount] = useState(5)
+    // const handleIncrease = () => {
+    //     setDetails((prev) => {
+    //         return {
+    //             ...prev,
+    //             counter : details.counter + 1
+    //         }
+    //     })
+    // }
+    // useEffect(() => {
+    //     document.title = `${details.counter} new message`
+    // }, [])
+    // // useEffect with a variable
+    // useEffect(() => {
+    //     document.title = `${otherCount} new message`
+    // }, [otherCount])
+    // const handleIncrease2  = () => {
+    //     setOtherCount(otherCount + 5)
+    // }
 
     useEffect(() => {
-        document.title = `${details.counter} new message`
-    }, [])
-    
+        
+        console.log("Run useEffect", count)
 
-    // useEffect with a variable
-    useEffect(() => {
-        document.title = `${otherCount} new message`
-    }, [otherCount])
+        return () => {
+            console.log("Clean Up", count)
+        }
+    }, [count])
 
 
-    const handleIncrease2  = () => {
-        setOtherCount(otherCount + 5)
-    }
+
     
     return (
         <div className=' text-3xl flex flex-col gap-3 justify-center items-center h-screen'>
-            <input className=' border-2' onBlur={(e) => setName(e.target.value)} type="text" />
+            {/* <input className=' border-2' onBlur={(e) => setName(e.target.value)} type="text" />
             <h2>{details.name} has click {details.counter} </h2>
             <Button onClick={handleIncrease}>Increase</Button>
             <h2>{details.name} has click {otherCount} </h2>
-            <Button onClick={handleIncrease2}>Increase</Button>
+            <Button onClick={handleIncrease2}>Increase</Button> */}
+
+            <h2>Count : {count}</h2>
+            <Button onClick={() => setCount(count + 1)}>Increase</Button>
         </div>
     );
 };
