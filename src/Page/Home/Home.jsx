8b9380506d3cfe0a211de2d5371aadbd805fc3f0
safinhr <hrsafin2434@gmail.com>
2023-12-24 19:03:58 +0600

    simple count for understanding useRecuer

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,47 +1,27 @@
 
-import React, { useEffect, useRef, useState } from 'react';
+import { Button } from '@material-tailwind/react';
+import React, {  useState } from 'react';
 
 const Home = () => {
-    // const [count, setCount] = useState(0)
-    const [name, setName] = useState("")
-    const inputElement = useRef(0)
-    // const [details, setDetails] = useState({counter : 0, name : "your text"})
-    // const [otherCount, setOtherCount] = useState(5)
-    // const handleIncrease = () => {
-    //     setDetails((prev) => {
-    //         return {
-    //             ...prev,
-    //             counter : details.counter + 1
-    //         }
-    //     })
-    // }
-    // useEffect(() => {
-    //     document.title = `${details.counter} new message`
-    // }, [])
-    // // useEffect with a variable
-    // useEffect(() => {
-    //     document.title = `${otherCount} new message`
-    // }, [otherCount])
-    // const handleIncrease2  = () => {
-    //     setOtherCount(otherCount + 5)
-    // }
+    
+    const [count, setCount] = useState(0)
 
-
-    const handleRef = () => {
-        console.log(inputElement)
-        inputElement.current.style.width = "800px"
-        inputElement.current.style.height = "200px"
-        inputElement.current.focus()
+    const increaseCount = () => {
+        setCount(prev => prev + 1)
     }
-    
 
+    const decreaseCount = () => {
+        setCount(prev => prev -1)
+    }
 
-
-    
     return (
-        <div className=' text-3xl flex flex-col gap-3 justify-center items-center h-screen'>
-            <input className=' border-2' type="text"  ref={inputElement} />
-           <button className=' py-2 px-3 rounded-md text-white bg-black' onClick={handleRef} >Click Me</button>
+        <div className=' text-3xl flex flex-col   gap-3 justify-center items-center h-screen'>
+            <h2>Count : {count}</h2>
+            <div className=' flex gap-5'>
+            <Button onClick={increaseCount}  color='green'>Increase</Button>
+            <Button onClick={decreaseCount}  color='red'>Decrease</Button>
+            </div>
+            
         </div>
     );
 };
